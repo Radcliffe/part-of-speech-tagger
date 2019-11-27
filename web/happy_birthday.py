c68c3bb2f68a69d1014bec73706a113de3224b01
David Radcliffe <dradcliffe@gmail.com>
2019-11-27 17:12:12 -0600

    remove example script

--- web/happy_birthday.py
+++
@@ -1,9 +0,0 @@
-# filename: happy_birthday.py
-"""A basic (single function) API written using hug"""
-import hug
-
-
-@hug.get('/happy_birthday')
-def happy_birthday(name, age:hug.types.number=1):
-    """Says happy birthday to a user"""
-    return "Happy {age} Birthday {name}!".format(**locals())
